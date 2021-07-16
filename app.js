cd6115102e8742dbdcbd072dda61ca4d9542bc7a
Pratik <pratikwadhai001@gmail.com>
2021-07-16 23:08:30 +0530

    Inital commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const { fstat } = require("fs");
+const path = require("path")
+const fs = require("fs")
+const app = express();
+const port = 80;
+
+
+//EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // for serving static files
+app.use(express.urlencoded())
+
+
+//PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // set the view directory
+
+// ENDPOINTS 
+app.get('/', (req, res) => {
+    const con = "This is the best content on the internet so far so use it wisely"
+    const params = { 'title': 'Gym Membership', "content": con }
+    res.status(200).render('index.pug', params)
+})
+
+app.post('/', (req, res) => {
+    name = req.body.name
+    age = req.body.age
+    gender = req.body.gender
+    address = req.body.address
+    more = req.body.more
+    let outputToWrite = `The name of the client is ${name}, ${age} years old, ${gender}, residing at ${address}. More about him/her:${more}`
+    fs.writeFileSync('output.txt', outputToWrite)
+    const params = { 'title': 'Your form has been submitted successfully', }
+    res.status(200).render('index.pug', params)
+
+})
+
+// START THE SERVER
+app.listen(port, () => {
+    console.log(`The application started successfully on port ${port}`)
+})
